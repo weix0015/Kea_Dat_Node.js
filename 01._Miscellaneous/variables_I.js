0e5f8b6c55670196f334e7712aab77251e3a4e1c
Wei Yang <weix0015@stud.kea.dk>
2024-02-02 09:35:01 +0100

    updated

---
+++ 01._Miscellaneous/variables_I.js
@@ -0,0 +1,13 @@
+// we will always default to become
+const myName = "Anders";
+
+
+const me = {
+};
+me.name = myName;
+console.log(me);
+
+// const is constant NOT in the value but in the assignment
+// meaning that it cannot be reassigned and thus also HAS to be assigned
+
+
