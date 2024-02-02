17ec4ecbec64dd9ec25b46a862f0e5153a3af15d
Wei Yang <weix0015@stud.kea.dk>
2024-02-02 10:25:33 +0100

    added Basic.js

--- 01._Miscellaneous/variables_I.js
+++ 01._Miscellaneous/variables_I.js
@@ -5,7 +5,6 @@ const myName = "Anders";
 const me = {
 };
 me.name = myName;
-console.log(me);
 
 // const is constant NOT in the value but in the assignment
 // meaning that it cannot be reassigned and thus also HAS to be assigned
@@ -21,4 +20,22 @@ me.hobbies = hobbies;
 
 me.hobbies = hobbies;
 
-console.log(me);
+// console.log(me);
+
+
+// Types in JS: number, string, boolean, object, null, undefined, symbol, bigint
+
+// Object: object, arrays, date
+
+// type coercion
+
+// we ALWAYS use strict equality checks:
+// === and !==
+
+// Strings:
+
+const stringOne = "This is one ''''''' '''way of defining strings";
+const stringTwo = 'This is one """" way """ of defining strings';
+const stringThree = `This """" '''' is one way of defining strings
+multi line string
+and here`;
---
+++ 02._Introduction/02._Introduction_Exercise/Basic.js
@@ -0,0 +1,36 @@
+// --------------------------------------
+// Variables, strings, numbers, floats
+// --------------------------------------
+// Exercise 1 - Console and constiables
+
+const firstName = "Anders";
+const lastName = "Latif";
+// EXERCISE
+// show in the console
+// My first name is Anders and my last name is Latif
+
+const fullName = `${firstName} ${lastName}`;
+
+console.log(fullName);
+
+// --------------------------------------
+// Exercise 2 - Numbers and Strings
+
+const year = "2023";
+const increment = 1;
+
+// Add the year plus the increment
+// The result should be 2024
+// You cannot touch line 1 or 2
+
+//const currentYear = parseInt(year) + increment;
+//const currentYear = Number(year) + increment;
+const currentYear = +year + increment;
+
+console.log(currentYear);
+
+console.log(Number("1231af324"));
+console.log(parseInt("1231af324"));
+
+
+// --------------------------------------
