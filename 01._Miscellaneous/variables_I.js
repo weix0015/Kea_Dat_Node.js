ce237324a5ae457064fe3592745964d62b635ec7
Wei Yang <weix0015@stud.kea.dk>
2024-02-02 09:41:17 +0100

    create variables

--- 01._Miscellaneous/variables_I.js
+++ 01._Miscellaneous/variables_I.js
@@ -10,4 +10,15 @@ console.log(me);
 // const is constant NOT in the value but in the assignment
 // meaning that it cannot be reassigned and thus also HAS to be assigned
 
+const hobbies = ["coding", "eating"];
 
+hobbies.push("sleeping");
+
+me.hobbies = hobbies;
+
+// hobbies = [];
+// hobbies = "";
+
+me.hobbies = hobbies;
+
+console.log(me);
