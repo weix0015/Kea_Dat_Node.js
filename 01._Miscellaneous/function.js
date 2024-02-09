6f0afe2f1eef11f5f27181220c6803ca61fd656a
Wei Yang <weix0015@stud.kea.dk>
2024-02-09 09:44:07 +0100

    added function.js

---
+++ 01._Miscellaneous/function.js
@@ -0,0 +1,36 @@
+// hoisting
+// getRandomInt();
+
+function getRandomInt(min, max) {
+    Math.floor(Math.random() * (max + 1 - min) + min);
+}
+
+const getRandomIntAnonymousFunction = function (min, max) {
+    return Math.floor(Math.random() * (max + 1 - min) + min);
+};
+
+const getRandomIntArrowFunction = (min, max) => {
+    return Math.floor(Math.random() * (max + 1 - min) + min);
+};
+
+// task write the above arrow function as a one-liner
+const getRandomIntArrowFunctionOneLiner = 
+    (min, max) => Math.floor(Math.random() * (max + 1 - min) + min);
+
+                            // string // callback function
+function genericActionPerformer(name, genericAction) {
+    return genericAction(name);
+}
+
+const running = (name) => `${name} is running`;
+
+// assignment write code below so that you console log "Alex is running"
+// const runningResult = genericActionPerformer("Alex", running);
+const runningResult = genericActionPerformer(running, "Alex");
+
+// console.log(runningResult);
+
+// assignment create a single line below that print "Louis is eating"
+console.log(genericActionPerformer("Louis", (name) => `${name} is eating`));
+
+
---
+++ 01._Miscellaneous/variables_II.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// totalGlobalVariable = "Never EVER!!! do this!!!";
+var globalVariable = "Also NEVER ever do this!!!";
+
+// use cconst whenever possible, else use let
+
+// about const:
+// const cannot be undeclared or redeclared
+//  but its value can be changed as long as you don't change the assignment
+
+
+// gloal scope
+
+function someOtherScope() {
+  // function scope
+}
+
+{
+  // block scope
+  console.log("What is this?");
+}
+
+{
+  let someValue = true;
+  {
+    let someValue = false;
+  }
+  console.log(someValue);
+}
+
+
+for (var i = 0; i <= 5; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, 1000);
+}
+
+for (let i = 0; i <= 5; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, 1000);
+}
