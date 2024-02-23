25662dcd67a94a31609040b1b2779e2e09690de8
Wei Yang <weix0015@stud.kea.dk>
2024-02-23 11:28:41 +0100

    implemented greeting feature

--- 04._First_HTML_Server/app.js
+++ 04._First_HTML_Server/app.js
@@ -12,6 +12,32 @@ app.get("/publicSquare", (req, res) => {
     res.sendFile(__dirname + "/public/publicSquare/publicSquare.html");
 });
 
+// take a name from the query string and greet the person if you know them
+// task otherwise say, "hello stranger"
+
+
+const knownName = "Wei"
+
+app.get("/greeting", (req, res) => {
+  if (req.query.name === knownName) {
+    res.send({ data: `Hello ${knownName}` })
+  } else {
+    res.send({ data: "Hello stranger!" })
+  }
+});
+
+/*
+const names = ["wei", "mikkel"]
+
+app.get("/greeting/:name", (req, res) => {
+  const value = req.params.name;
+  if (value === names[0] || value === names[1]) {
+    res.send({ data: "Hello" });
+  } else {
+    res.send({ data: "hello stranger"});
+  }
+});
+*/
 
 
 const PORT = 8080;
