c69c3872acdb5399129b82a6d092ede24e418411
Wei Yang <weix0015@stud.kea.dk>
2024-03-01 13:21:11 +0100

    finished DogInder

--- 06._DogInder/app.js
+++ 06._DogInder/app.js
@@ -6,8 +6,29 @@ app.use(express.static("public"));
 
 import path from "path";
 
+import getMatches from "./util/matches.js";
+
+// ======= HTML =============
+
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve("public/homepage/homepage.html"));
+  res.sendFile(path.resolve("public/pages/homepage/homepage.html"));
+});
+
+app.get("/matches", (req, res) => {
+  res.sendFile(path.resolve("public/pages/matches/matches.html"));
+});
+
+// assignment create a contact page
+
+app.get("/contact", (req, res) => {
+  res.sendFile(path.resolve("public/pages/contact/contact.html"));
+})
+
+// ======= API =============
+
+app.get("/api/matches", (req, res) => {
+  const matches = getMatches();
+  res.send({ data: matches });
 });
 
 const PORT = 8080;
