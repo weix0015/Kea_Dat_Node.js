b4de3cb8ebb8f2291887ce8c5cc1cd0bc191895c
Wei Yang <weix0015@stud.kea.dk>
2024-03-15 08:51:32 +0100

    added debug and operator

--- 06._DogInder/util/templatingEngine.js
+++ 06._DogInder/util/templatingEngine.js
@@ -5,12 +5,18 @@ export function readPage(path) {
   return fs.readFileSync(path).toString();
 }
 
+// nullish coalesce operator ??  ||
+// const value = "";
+
+// console.log(value ?? "some other value");
+// console.log(value || "some other value"); // type coerces... takes falsy values and coerce
+
 const footer = fs.readFileSync("./public/components/footer/footer.html").toString();
 
 export function renderPage(page, config={}) {
   const header = fs.readFileSync("./public/components/header/header.html").toString();
-  return header.replace("$TAB_TILES$", config.tabTile || "DogInder")
-  .replace("$CSS_LINKS$", config.CSSLinks || "")
+  return header.replace("$TAB_TILES$", config.tabTile ?? "DogInder")
+  .replace("$CSS_LINKS$", config.CSSLinks ?? "")
   + page 
   + footer;
 }
